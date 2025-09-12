a727e9e6f8b510d505cc74a1d54746ecc33d49df
JamesG <jangla.mail@gmail.com>
2025-09-12 22:55:39 +0100

    fix lockfile

--- src/components/ui/fancy-multi-select.tsx
+++ src/components/ui/fancy-multi-select.tsx
@@ -153,7 +153,7 @@ export function FancyMultiSelect({
                         e.preventDefault();
                         e.stopPropagation();
                       }}
-                      onSelect={(value) => {
+                      onSelect={() => {
                         setInputValue("");
                         handleSelect(option);
                       }}
